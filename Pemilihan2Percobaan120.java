f3e22f9a0ce548a7b9fef398a85b102e05db0083
Your Name <you@example.com>
2023-10-03 11:51:05 +0700

    jobsheet6

--- Pemilihan2Percobaan120.java
+++ Pemilihan2Percobaan120.java
@@ -10,8 +10,10 @@ public class Pemilihan2Percobaan120 {
 
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) != 0) 
             System.out.println("Tahun Kabisat");
+            else
+            System.out.println("Bukan Tahun Kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat");
     }
